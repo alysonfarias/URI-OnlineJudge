95252cea8cc4f5140f7484bd5ab19ed3c1a8ba3a
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2022-03-16 19:40:03 -0300

    Create TwoSumLeetCode.cs

---
+++ TwoSumLeetCode.cs
@@ -0,0 +1,77 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+*/
+
+//Not optimized search O(n^2)
+
+public class Solution {
+    public int[] TwoSum(int[] array, int target) {
+            if (array == null || array.Length < 2) {
+                return Array.Empty < int > ();
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if(j != i)
+                    {
+                        if (array[i] + array[j] == target)
+                        {
+                            return new int[2] { i, j };
+                        }
+                    }
+
+                }
+            }
+            return Array.Empty < int > ();
+    }
+    public int[] TwoSumOptimized(int[] array, int target) {
+        int arrayLength = array.Length;
+        if (array == null || arrayLength < 2) {
+            return Array.Empty < int > ();
+        }
+        Dictionary < int, int > resultDictionary = new();
+
+        for (int i = 0; i < arrayLength; i++) {
+            int firstNumber = array[i];
+            int secondNumber = target - firstNumber;
+            if (resultDictionary.TryGetValue(secondNumber, out int index)) {
+                return new [] {
+                    index,
+                    i
+                };
+            }
+            //resultDictionary.Add(firstNumber, i);
+            resultDictionary[firstNumber] = i;
+        }
+        return Array.Empty < int > ();;
+    }
+}
+
+//Optimezed search O(n)
+
+
+
