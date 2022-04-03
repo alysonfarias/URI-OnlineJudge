1d2faa5bccd28a58fa6d088607a5937341aa8da4
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2022-04-03 20:06:26 -0300

    Create ContainsDuplicate.cs

---
+++ ContainsDuplicate.cs
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/contains-duplicate
+
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+*/
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> hashSetValues = new HashSet<int>();
+        
+        for(int id=0; id<nums.Length; id++){
+            if (hashSetValues.Contains(nums[id])){
+                return true;
+            }
+            hashSetValues.Add(nums[id]);            
+        }        
+        return false;
+    }
+}
