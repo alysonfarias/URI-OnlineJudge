25f08cb0e830b0d15b3496d0fdb50a306200763a
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2022-04-17 23:07:14 -0300

    Create 584FindCustomerReferee.sql

---
+++ 584FindCustomerReferee.sql
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/find-customer-referee/submissions/
+# Write your MySQL query statement below
+SELECT
+    name
+FROM
+    customer
+WHERE
+    referee_id <> 2 OR referee_id IS NULL
