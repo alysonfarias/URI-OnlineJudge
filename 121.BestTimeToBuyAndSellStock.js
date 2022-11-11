98e545528659a2470c5e6e11cd032504043c44a7
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2022-11-10 22:13:34 -0300

    Create 121.BestTimeToBuyAndSellStock.js

---
+++ 121.BestTimeToBuyAndSellStock.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+	let profit = 0;
+
+	let stockToBuy = prices[0];
+
+	for (let i = 1; i < prices.length; i++) {
+		if (stockToBuy > prices[i])
+			stockToBuy = prices[i];
+
+		const currentProfit = prices[i] - stockToBuy;
+
+		if (currentProfit > profit)
+			profit = currentProfit;
+	}
+
+	return profit;
+};
