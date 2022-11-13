979fbb3b031adf589bf17c71f0a949bd38012829
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2022-11-12 21:18:05 -0300

    Create MergeTwoSortedList.js

---
+++ MergeTwoSortedList.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/submissions/
+// using recursion
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+};
