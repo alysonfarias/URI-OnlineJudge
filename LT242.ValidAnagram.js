edb3df93f3995aaadb3e15539f6adffe9e9020d3
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2022-11-18 22:50:21 -0300

    Create LT242.ValidAnagram.js

---
+++ LT242.ValidAnagram.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/valid-anagram/description/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    return s.split('').sort().join('') === t.split('').sort().join('');
+};
